75cc1f6fd3669ae9b46d257f26d95cfb5eb30d25
Harshil Jani <harshiljani2002@gmail.com>
2023-03-02 00:23:44 +0530

    Added tests
    Signed-off-by: Harshil Jani <harshiljani2002@gmail.com>

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include<cmath>
 #include<vector>
 #include<fstream>
+#include <cassert>
 
 // Vector class which can be useful for taking x,y,z and applying vector properties on them.
 class Vector {
@@ -51,7 +52,7 @@ class FourVector: public Vector {
 
         // Calculating the mass of given vector.
         double mass_of_vector(){
-            return std::sqrt(e_vector_value()*e_vector_value()-((x_vector_value()*x_vector_value()-y_vector_value()*y_vector_value()-z_vector_value()*z_vector_value()))/3*10e8);
+            return std::sqrt(e*e-(pow(x_vector_value(),2)+pow(y_vector_value(),2)+pow(z_vector_value(),2)));
         }
 };
 
@@ -232,6 +233,53 @@ class Output{
         }
 };
 
+
+void test_all() {
+
+    // Test Vector class
+    Vector v(1.0, 2.0, 3.0);
+    assert(v.x_vector_value() == 1.0);
+    assert(v.y_vector_value() == 2.0);
+    assert(v.z_vector_value() == 3.0);
+    assert(v.magnitude() == std::sqrt(14.0));
+
+    // Test FourVector class
+    FourVector fv(1.0, 2.0, 3.0, 4.0);
+    assert(fv.x_vector_value() == 1.0);
+    assert(fv.y_vector_value() == 2.0);
+    assert(fv.z_vector_value() == 3.0);
+    assert(fv.e_vector_value() == 4.0);
+    assert(fv.mass_of_vector() == std::sqrt(2.0));
+
+    // Test Reader class
+    Reader reader("test.dat");
+    std::vector<FourVector> data = reader.readFile();
+    assert(data.size() == 3);
+    assert(data[0].x_vector_value() == 1.0);
+    assert(data[0].y_vector_value() == 2.0);
+    assert(data[0].z_vector_value() == 3.0);
+    assert(data[0].e_vector_value() == 4.0);
+    assert(data[1].x_vector_value() == 5.0);
+    assert(data[1].y_vector_value() == 6.0);
+    assert(data[1].z_vector_value() == 7.0);
+    assert(data[1].e_vector_value() == 8.0);
+    assert(data[2].x_vector_value() == 9.0);
+    assert(data[2].y_vector_value() == 10.0);
+    assert(data[2].z_vector_value() == 11.0);
+    assert(data[2].e_vector_value() == 12.0);
+
+    // Test Output class
+    Output output(data, 10, 0.0, 20.0, "pT");
+    assert(output.getDistributionIndex() == 0);
+    assert(output.distribution_pT(data.begin()) == std::sqrt(14.0));
+    assert(output.distribution_pX(data.begin()) == 1.0);
+    assert(output.distribution_pY(data.begin()) == 2.0);
+    assert(output.distribution_pZ(data.begin()) == 3.0);
+    assert(output.distribution_energy(data.begin()) == 4.0);
+    assert(output.distribution_mass(data.begin()) == std::sqrt(2.0));
+}
+
+
 int main(){
 
     std::cout<<"Four Vector Analysis\n";
@@ -248,6 +296,10 @@ int main(){
     std::cin>>distribution;
     std::cout<<"Number of Bins : ";
     std::cin>>number_of_bins;
+    if(number_of_bins==0){
+        std::cout<<"Number of Bins cannot be 0\n";
+        exit(1);
+    }
     std::cout<<"Enter X(min) for the histogram : ";
     std::cin>>xmin;
     std::cout<<"Enter X(max) for the histogram : ";
@@ -261,5 +313,8 @@ int main(){
     Output output(data,number_of_bins,xmin,xmax,distribution);
     // Calling out the output system for running Histogram simulation.
     output.histogram();
+
+    // Running tests to verify the working for functions.
+    test_all();
     return 0;
 }
